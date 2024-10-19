7a466a1d958d2ba6cab3897e0b60b6a04e0f3b0e
Chris <chye0001@stud.kea.dk>
2024-10-19 12:17:24 +0200

    created util.js

---
+++ javascript/util.js
@@ -0,0 +1,57 @@
+export function checkHttpErrors(response) {
+    if (!response.ok) {
+        const errorResponse = response.json();
+        const error = new Error(errorResponse.message);
+        error.apiError = errorResponse;
+        error.status = response.status;
+        throw error;
+    }
+}
+
+
+
+
+
+export function makeOption(httpMethod, body) {
+
+    const validHttpMethods = ["GET", "POST", "PUT", "DELETE"];
+
+    if (!validHttpMethods.includes(httpMethod)) {
+        throw new Error(`Invalid HTTP method: ${httpMethod}`)
+    }
+
+    const option = {
+        method: httpMethod.toUpperCase(),
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        }
+    }
+
+    if (body) {
+        option.body = JSON.stringify(body);
+    }
+
+    return option;
+}
+
+
+
+
+
+export function makeAuthOption(httpMethod, body, token) {
+
+    const option = {
+        method: httpMethod.toUpperCase(),
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + token
+        }
+    }
+
+    if (body) {
+        option.body = JSON.stringify(body);
+    }
+
+    return option;
+}
