f003d9e6d100d1c4fa7d8b0f684ca04a075ed01d
Chris <chye0001@stud.kea.dk>
2024-10-19 16:57:26 +0200

    populated home.js with logic

--- javascript/home.js
+++ javascript/home.js
@@ -1,14 +1,16 @@
-import {getDecodedToken} from "./util.js";
+import {getDecodedToken, NavbarComponent} from "./util.js";
 
 export function initHomeScript() {
+
+    const homeNavbar = document.getElementById("home-navbar");
+    homeNavbar.innerHTML = NavbarComponent();
 
     const decodedToken = getDecodedToken();
     const username = decodedToken.sub;
 
-    const homePage = document.getElementById("home");
     const welcomeMessage = document.getElementById("home-welcome-message");
-
     welcomeMessage.innerText = `Welcome back, ${username}!`
 
-    const navbar = document.getElementById("navbar");
+    //TODO
+    //think of other content that could go onto the home page.
 }
